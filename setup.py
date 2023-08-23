f2160a8ef55ae444582d1ee9fb740e3c4f7f4a6c
Anup Kumar Sahu <anup-kumar.sahu@hpe.com>
2023-08-23 10:24:19 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Deep-Learning-with-TensorFlow-and-Keras---Third-Edition"
+AUTHOR_USER_NAME = "anupkumarsahu"
+SRC_REPO = "deepLearning"
+AUTHOR_EMAIL = "anup.sahu@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Learning tensorflow and keras on Deep-Learning",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
